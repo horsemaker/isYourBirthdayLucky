1d4d0b0730a0ea12fc93d7248aa56d74b63479f2
horsemaker <yash_ghodekar@yahoo.in>
2021-07-27 18:48:07 +0530

    app complete

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var birthDateInput = document.querySelector('#birth-date')
 var luckyNoInput = document.querySelector('#lucky-no')
 var checkAction = document.querySelector('#check')
+var finalResult = document.querySelector('#final-result')
 var result = document.querySelector('#result')
+var privacyNote = document.querySelector('#privacy-note')
+var closeNote = document.querySelector('#close-note')
 
-function isYourBirthdayLucky() {
+var finalImg = document.createElement('img')
+finalImg.className = "finalImg"
+
+function isYourBirthdayLucky(event) {
+  event.preventDefault()
+
+  if (!birthDateInput.value || !luckyNoInput.value) {
+    alert("Please enter correct values to proceed!")
+    return
+  } else if (parseInt(luckyNoInput.value) === 0) {
+    alert("Lucky Number can't be 0! :)")
+    return
+  }
+
   let birthDate = parseInt(birthDateInput.value.split('-').join(''))
   let luckyNo = parseInt(luckyNoInput.value)
 //   console.log("Hello!")
@@ -24,11 +40,21 @@ function isYourBirthdayLucky() {
   if (dateSum % luckyNo === 0) {
     // console.log("Yes, your birthday is lucky! :)")
     result.textContent = 'Yes, your birthday is lucky! :)'
+    finalImg.src = 'assets/happy.gif'
   }
   else {
     // console.log("No, your birthday is not lucky! :(")
     result.textContent = 'No, your birthday is not lucky! :('
+    finalImg.src = 'assets/sad.gif'
   }
+  finalResult.appendChild(finalImg)
+}
+
+function closePrivacyNote() {
+  // console.log(privacyNote)
+  // console.log(closeNote)
+  privacyNote.style.display = "none"
 }
 
 checkAction.addEventListener('click', isYourBirthdayLucky)
+closeNote.addEventListener('click', closePrivacyNote)
